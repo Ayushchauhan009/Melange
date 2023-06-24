36725163f50c112b0a70e98f55baa8ad24ecff6a
aayushchauhan10 <ai.aayush10@gmail.com>
2023-06-24 14:07:21 +0530

    Dummy Live

--- src/components/layout/Clientele.jsx
+++ src/components/layout/Clientele.jsx
@@ -13,7 +13,7 @@ const Client = ({ icon }) => {
 
 const Clientele = () => {
   return (
-    <div className="font-nunito pb-8 bg-[#1a1a1a]  mt-32">
+    <div className="font-nunito pb-8 2xl:pb-12 bg-[#1a1a1a]  mt-32 2xl:mt-44">
       <div className="mt-16 px-4 py-8 md:py-16 md:px-24 lg:px-28">
         <h1 className="text-[#fff] text-2xl md:text-3xl lg:text-4xl font-bold">
           Clientele

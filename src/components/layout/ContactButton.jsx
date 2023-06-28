94e0820d72f3fa36cf586479ba1c35af3eefb6ff
aayushchauhan10 <ai.aayush10@gmail.com>
2023-06-28 15:26:25 +0530

    Dummy Live

--- src/components/layout/ContactButton.jsx
+++ src/components/layout/ContactButton.jsx
@@ -5,15 +5,19 @@ import { Link } from "react-router-dom";
 const ContactButton = () => {
   return (
     <div className="my-3">
-      <Link to="/contact" className="expand-button font-nunito">
-        <span className="button-text font-bold text-[#0f0330]">Contact Us</span>
-        <span className="arrow-container">
-          <img
-            src={rightArrow}
-            alt="Arrow"
-            className="w-5  h-6 mt-0.5 mr-0.5 arrow-image"
-          />
-        </span>
+      <Link to="/contact">
+        <button className="expand-button font-nunito">
+          <span className="button-text font-bold text-[#0f0330]">
+            Contact Us
+          </span>
+          <span className="arrow-container">
+            <img
+              src={rightArrow}
+              alt="Arrow"
+              className="w-5  h-6 mt-0.5 mr-0.5 arrow-image"
+            />
+          </span>
+        </button>
       </Link>
     </div>
   );
